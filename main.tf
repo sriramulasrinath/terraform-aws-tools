a1e63c618803e50fa5d509ea6c640dbb8f959377
srinath sriramula <sriramulasrinath59@gmail.com>
2024-07-11 10:36:53 +0530

    commiting changes

--- main.tf
+++ main.tf
@@ -18,7 +18,6 @@ module "jenkins_agent" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
   name = "jenkins-agent"
-
   instance_type          = "t3.small"
   vpc_security_group_ids = ["sg-00d8e884e38dae954"]
   subnet_id              = "subnet-09e5fbac6203f2585"
@@ -30,6 +29,33 @@ module "jenkins_agent" {
     }
 }
 
+
+resource "aws_key_pair" "tools" {
+  key_name = "tools"
+  #public_key =  ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF3/FINvjYpjY9kIAHD9dNsURR4KTBpqdCdmYJjXLSBU
+  public_key = file("~/.ssh/tools.pub")
+}
+
+module "nexus" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "nexus"
+
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = ["sg-00d8e884e38dae954"]
+  subnet_id              = "subnet-09e5fbac6203f2585"
+  key_name = aws_key_pair.tools.key_name
+  ami                    = data.aws_ami.nexus_ami_info.id
+  root_block_device = [
+    {
+      volume_type = "gp3"
+      volume_size = 30
+    }
+  ]
+  tags = {
+    Name = "nexus"
+  }
+}
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
@@ -51,6 +77,14 @@ module "records" {
       ttl     = 1
       records = [
         module.jenkins_agent.private_ip
+      ]
+    },
+    {
+      name    = "nexus"
+      type    = "A"
+      ttl     = 1
+      records = [
+        module.nexus.private_ip
       ]
     }
   ]

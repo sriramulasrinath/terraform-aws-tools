9fbda9b796640856405210e439cafe5803bbdfc5
srinath sriramula <sriramulasrinath59@gmail.com>
2024-07-19 15:59:07 +0530

    commiting changes

--- jenkins-agent.sh
+++ jenkins-agent.sh
@@ -19,3 +19,9 @@ systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
 VALIDATE $? "Docker installation"
+
+# Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+VALIDATE $? "helm installation"

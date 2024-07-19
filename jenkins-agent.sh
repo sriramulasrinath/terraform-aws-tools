8766ee214bf545d5708e13af4a9cb5df1b36d3e9
srinath sriramula <sriramulasrinath59@gmail.com>
2024-07-19 15:42:48 +0530

    commiting changes

--- jenkins-agent.sh
+++ jenkins-agent.sh
@@ -10,3 +10,12 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 yum install zip -y
+
+# docker
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
+VALIDATE $? "Docker installation"
